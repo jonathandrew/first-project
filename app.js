a62e442bd7ea5a28209899b35c268938cdb10d21
Jonathan Drew <jonathan.drew@codeimmersives.com>
2020-03-17 15:50:33 -0400

    got api along with key need to plug in and display the info somewhere

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const fetch = require("fetch");
 
 require("dotenv").config();
 
--- routes/home.js
+++ routes/home.js
@@ -1,10 +1,12 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const fetch = require("fetch");
 
 /* GET home page. */
 
 router.get("/", function(req, res, next) {
   res.render("home");
 });
+router.get("/login", (req, res) => {});
 
 module.exports = router;
