57269028ff41a91004f0f7abe0778f3f71a860b8
Jonathan Drew <jonathan.drew@codeimmersives.com>
2020-03-23 10:32:31 -0400

    final commit

--- routes/fighter/controller/fighterController.js
+++ routes/fighter/controller/fighterController.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 const api_key = process.env.API_KEY;
-const competitor_ID = process.env.COMPETITOR_ID;
+let competitor_ID = process.env.COMPETITOR_ID;
 
 const url = `http://api.sportradar.us/ufc/trial/v2/en/competitors/${competitor_ID}/profile.json?api_key=${api_key}`;
 module.exports = {
@@ -8,14 +8,23 @@ module.exports = {
     axios
       .get(url)
       .then(info => {
-        let information = info.data.info;
-        let record = info.data.record;
-        let competitor = info.data.competitor;
+        if (competitor_ID === competitor_ID) {
+          let competitor = info.data.competitor;
+          let information = info.data.info;
+          let record = info.data.record;
+
+          competitor_ID = competitor.id;
+          res.render("fighters/fighters", {
+            information,
+            record,
+            competitor
+          });
+        }
+        // let competitor = info.data.competitor;
         // let ranking = info.info;
         console.log("LENGTH...", info.data.info);
         console.log("test test test", info.data.record);
         console.log("-------", info);
-        res.render("fighters/fighters", { information, record, competitor });
         // console.log(info.data.rankings);
       })
       .catch(err => {
