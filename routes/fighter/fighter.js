2769733ce67ad4baa615440ca7684ef34227edeb
Jonathan Drew <jonathan.drew@codeimmersives.com>
2020-03-29 14:28:10 -0400

    i am lost in th =e sauce, 99% sure what im going is supposed to be done in a different manner

--- routes/fighter/fighter.js
+++ routes/fighter/fighter.js
@@ -9,11 +9,25 @@ router.get("/single-fighter/:id", fighterController.fighters);
 // router.get("/mylist", fighterController.mylist);
 
 router.get("/mylist/:fighter/:fighter_id", (req, res, next) => {
-  let fighter = req.params.fighters;
-  let fighter_id = req.params.fighteer_id;
+  let fighter = req.params.fighter;
+  let fighter_id = req.params.fighter_id;
   console.log(fighter);
   console.log(fighter_id);
   res.render("fighters/mylist", { fighter, fighter_id });
+});
+router.post("/mylist", (req, res, next) => {
+  const userList = new User();
+  userList.fighter = req.body.favorite;
+  userList.fighter_id = req.body.fighter_id;
+  userList
+    .save()
+    .then(info => {
+      console.log(info);
+      res.redirect("/mylist/", { userlist });
+    })
+    .catch(err => {
+      return res.status(500).json({ err: err });
+    });
 });
 
 module.exports = router;
