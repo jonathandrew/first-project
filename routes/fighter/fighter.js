0f2b576a78dbd925bb57b954bb3861a2f05dc38c
Jonathan Drew <jonathan.drew@codeimmersives.com>
2020-04-01 15:44:40 -0400

    testing readme

--- routes/fighter/fighter.js
+++ routes/fighter/fighter.js
@@ -2,8 +2,6 @@ const express = require("express");
 const router = express.Router();
 const fighterController = require("./controller/fighterController");
 // Make a request for a user with a given ID
-const User = require("../user/models/User");
-
 router.get("/single-fighter/:id", fighterController.fighters);
 
 // router.get("/mylist", fighterController.mylist);
